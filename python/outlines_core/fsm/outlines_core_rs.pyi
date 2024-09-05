729e922e84110d5d8f85ad1d0134cb4805b94703
drbh <david.richard.holtz@gmail.com>
2024-09-05 19:56:21 -0400

    Initial to_fsm logic for core regex elements

--- python/outlines_core/fsm/outlines_core_rs.pyi
+++ python/outlines_core/fsm/outlines_core_rs.pyi
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional, Set, Tuple
+from typing import Any, Dict, List, Optional, Set, Tuple
 
 class FSMInfo:
     initial: int
@@ -52,6 +52,7 @@ def create_fsm_index_end_to_end(
     vocabulary: List[Tuple[str, List[int]]],
     frozen_tokens: frozenset[str],
 ) -> Dict[int, Dict[int, int]]: ...
+def parse_pattern(pattern: str) -> Any: ...
 
 BOOLEAN: str
 DATE: str

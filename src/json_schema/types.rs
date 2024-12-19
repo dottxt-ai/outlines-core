fbab8a81d283d4b958697bb833213692d6d52500
Aakash Thatte <84656834+sky-2002@users.noreply.github.com>
2024-12-19 22:34:49 +0530

    Support URI format  (#129)
    Adds URI parsing code and tests
    
    Refer #127

--- src/json_schema/types.rs
+++ src/json_schema/types.rs
@@ -36,6 +36,7 @@ pub static TIME: &str = r#""(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\\.[0-9
 pub static UUID: &str = r#""[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}""#;
 pub static URI: &str = r#"^(https?|ftp):\/\/([^\s:@]+(:[^\s:@]*)?@)?([a-zA-Z\d.-]+\.[a-zA-Z]{2,}|localhost)(:\d+)?(\/[^\s?#]*)?(\?[^\s#]*)?(#[^\s]*)?$|^urn:[a-zA-Z\d][a-zA-Z\d\-]{0,31}:[^\s]+$"#;
 pub static EMAIL: &str = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+pub static URI: &str = r#"^(https?|ftp):\/\/([^\s:@]+(:[^\s:@]*)?@)?([a-zA-Z\d.-]+\.[a-zA-Z]{2,}|localhost)(:\d+)?(\/[^\s?#]*)?(\?[^\s#]*)?(#[^\s]*)?$|^urn:[a-zA-Z\d][a-zA-Z\d\-]{0,31}:[^\s]+$"#;
 
 #[derive(Debug, PartialEq)]
 pub enum FormatType {
@@ -45,6 +46,7 @@ pub enum FormatType {
     Uuid,
     Uri,
     Email,
+    Uri,
 }
 
 impl FormatType {
@@ -56,6 +58,7 @@ impl FormatType {
             FormatType::Uuid => UUID,
             FormatType::Uri => URI,
             FormatType::Email => EMAIL,
+            FormatType::Uri => URI,
         }
     }
 
@@ -68,6 +71,7 @@ impl FormatType {
             "uuid" => Some(FormatType::Uuid),
             "uri" => Some(FormatType::Uri),
             "email" => Some(FormatType::Email),
+            "uri" => Some(FormatType::Uri),
             _ => None,
         }
     }

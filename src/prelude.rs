60c79629ceae30602cd79fed0ef1129f9e2a90a8
Owen Shepherd <owen@owen.cafe>
2025-02-10 02:28:45 +0000

    Remove all the things...

--- src/prelude.rs
+++
@@ -1,8 +0,0 @@
-//! Library's interface essentials.
-
-pub use tokenizers::FromPretrainedParameters;
-
-pub use super::index::Index;
-pub use super::json_schema;
-pub use super::primitives::{StateId, Token, TokenId};
-pub use super::vocabulary::Vocabulary;

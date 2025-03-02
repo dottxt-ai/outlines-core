f22d400ac51795b704eda0df390b25bfe7dac01c
Nathan Hoos <thwackyy.y@gmail.com>
2025-03-01 19:23:17 -0600

    add mlx benchmarks.

--- benchmarks/bench_kernels.py
+++ benchmarks/bench_kernels.py
@@ -9,6 +9,15 @@ import torch
 from outlines_core.kernels.numpy import _apply_token_bitmask_kernel as np_kernel
 from outlines_core.kernels.torch import _apply_token_bitmask_kernel as torch_kernel
 
+def generate_sparse_mask(batch, vocab, allowed_count=1000):
+        mask_shape = (batch, (vocab + 31) // 32)
+        mask = np.zeros(mask_shape, dtype=np.int32)
+        allowed_indices = random.sample(range(vocab), allowed_count)
+        for idx in allowed_indices:
+            group = idx // 32
+            shift = idx % 32
+            mask[0, group] |= 1 << shift
+        return mask
 
 class TorchBitmaskApplyBench:
     params = [10, 100, 1000, 10000]
@@ -21,34 +30,19 @@ class TorchBitmaskApplyBench:
         self.vocab = 128000
 
         self.logits = torch.randn(1, self.vocab, device=self.device)
-        self.logits_ref = self.logits.clone()
 
-        mask = self._generate_sparse_mask(
+        mask = torch.from_numpy(generate_sparse_mask(
             1, self.vocab, allowed_count=self.allowed_tokens
-        )
+        ))
         self.mask = mask.to(self.device)
 
         self.kernel = torch_kernel
 
-        self.input_tensor = self.logits_ref.clone()
-
-        # warm up / compile
         for _ in range(4):
-            self.kernel(self.input_tensor, self.mask)
-            self.input_tensor.copy_(self.logits_ref)
-
-    def _generate_sparse_mask(self, batch, vocab, allowed_count=1000):
-        mask_shape = (batch, (vocab + 31) // 32)
-        mask = torch.zeros(mask_shape, dtype=torch.int32)
-        allowed_indices = random.sample(range(vocab), allowed_count)
-        for idx in allowed_indices:
-            group = idx // 32
-            shift = idx % 32
-            mask[0, group] |= 1 << shift
-        return mask
+            self.kernel(self.logits, self.mask)
 
     def time_kernel(self, allowed_tokens):
-        self.kernel(self.logits_ref, self.mask)
+        self.kernel(self.logits, self.mask)
 
 
 class NumpyBitmaskApplyBench:
@@ -61,30 +55,43 @@ class NumpyBitmaskApplyBench:
         self.vocab = 128000
 
         self.logits = np.random.randn(1, self.vocab).astype(np.float32)
-        self.logits_ref = self.logits.copy()
 
-        self.mask = self._generate_sparse_mask(
+        self.mask = generate_sparse_mask(
             1, self.vocab, allowed_count=self.allowed_tokens
         )
 
         self.kernel = np_kernel
 
-        self.input_array = self.logits_ref.copy()
+        for _ in range(4):
+            self.kernel(self.logits, self.mask)
+
+    def time_kernel(self, allowed_tokens):
+        self.kernel(self.logits, self.mask)
+
+
+class MlxBitmaskApplyBench:
+    params = [10, 100, 1000, 10000]
+    param_names = ["allowed_tokens"]
+    number = 10
+    disabled = True
+
+    def setup(self, allowed_tokens):
+        from outlines_core.kernels.mlx import _apply_token_bitmask_kernel as mlx_kernel
+        import mlx.core as mx
+        self.allowed_tokens = allowed_tokens
+        self.vocab = 128000
+
+        self.logits = mx.array(np.random.randn(1, self.vocab).astype(np.float32))
+
+        self.mask = mx.array(generate_sparse_mask(
+            1, self.vocab, allowed_count=self.allowed_tokens
+        ))
+
+        self.kernel = mlx_kernel
 
         # warm up / compile
         for _ in range(4):
-            self.kernel(self.input_array, self.mask)
-            self.input_array[:] = self.logits_ref
-
-    def _generate_sparse_mask(self, batch, vocab, allowed_count=1000):
-        mask_shape = (batch, (vocab + 31) // 32)
-        mask = np.zeros(mask_shape, dtype=np.int32)
-        allowed_indices = random.sample(range(vocab), allowed_count)
-        for idx in allowed_indices:
-            group = idx // 32
-            shift = idx % 32
-            mask[0, group] |= 1 << shift
-        return mask
+            self.kernel(self.logits, self.mask)
 
     def time_kernel(self, allowed_tokens):
-        self.kernel(self.logits_ref, self.mask)
+        self.kernel(self.logits, self.mask)

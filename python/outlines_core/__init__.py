418481ad072f3f748c6f31ea5b3aa28aa988a739
Lequn Chen <lequn@perplexity.ai>
2025-03-12 18:30:14 +0000

    Re-export public APIs in __init__.py

--- python/outlines_core/__init__.py
+++ python/outlines_core/__init__.py
@@ -1,7 +1,10 @@
 """This package provides core functionality for structured generation, formerly implemented in Outlines."""
+
 from importlib.metadata import PackageNotFoundError, version
 
-from .outlines_core_rs import Guide, Index, Vocabulary
+from .outlines_core_rs import Guide as Guide
+from .outlines_core_rs import Index as Index
+from .outlines_core_rs import Vocabulary as Vocabulary
 
 try:
     __version__ = version("outlines_core")

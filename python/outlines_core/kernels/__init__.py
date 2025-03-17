0ce1556182316c2be9536aa19dd91f8c2ac387ea
Nathan Hoos <thwackyy.y@gmail.com>
2025-03-17 00:19:09 +0000

    remove attempt at fixing benchmark workflow.

--- python/outlines_core/kernels/__init__.py
+++ python/outlines_core/kernels/__init__.py
@@ -1,9 +1,1 @@
-"""Kernel implementations for various backends."""
-
-from .numpy import _apply_token_bitmask_inplace_kernel as numpy_kernel
-from .torch import _apply_token_bitmask_inplace_kernel as torch_kernel
-
-try:
-    from .mlx import _apply_token_bitmask_kernel as mlx_kernel
-except ImportError:
-    pass
+"""Token Masking kernel implementations for various backends."""
